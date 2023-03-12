3b3a3e4bc8a52cb4e95a8c43090f04c420b5693e
Moiz Ahmed <moizahmedbss786@gmail.com>
2023-03-12 15:37:05 +0500

    Add files via upload

---
+++ Josephus Problem II.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int n, k;
+    cin >> n >> k;
+    indexed_set s;
+    for (int i = 1; i <= n; i++)
+        s.insert(i);
+
+    int ind = k % n;
+    while (n--) {
+        auto y = s.find_by_order(ind);
+        cout << *y << " ";
+        s.erase(y);
+        if (n) ind = (ind + k) % n;
+    }
+    cout << endl;
+    return 0;
+}
