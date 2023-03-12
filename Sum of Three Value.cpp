cb672c994928049d35aeab7b3d3e757c920ccb40
Moiz Ahmed <moizahmedbss786@gmail.com>
2023-03-12 15:37:32 +0500

    Add files via upload

---
+++ Sum of Three Value.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n, x;
+    cin >> n >> x;
+    vector<pair<int, int>> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i].first;
+        a[i].second = i + 1;
+    }
+    sort(a.begin(), a.end());
+    for (int i = 0; i < n - 2; i++) {
+        int j = i + 1, k = n - 1;
+        while (j < k) {
+            int sum = a[i].first + a[j].first + a[k].first;
+            if (sum == x) {
+                cout << a[i].second << " " << a[j].second << " " << a[k].second << endl;
+                return 0;
+            }
+            else if (sum < x) {
+                j++;
+            }
+            else {
+                k--;
+            }
+        }
+    }
+    cout << "IMPOSSIBLE" << endl;
+    return 0;
+}
